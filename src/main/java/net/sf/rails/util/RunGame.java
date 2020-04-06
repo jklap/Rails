d38707b24c9390497641a703a6674edc37107d14
Jason Klapste <jklapste@gmail.com>
2020-04-05 22:10:45 -0500

    mac file opening

--- src/main/java/net/sf/rails/util/RunGame.java
+++ src/main/java/net/sf/rails/util/RunGame.java
@@ -1,38 +1,52 @@
 package net.sf.rails.util;
 
+import java.awt.*;
+import java.awt.desktop.OpenFilesEvent;
+import java.awt.desktop.OpenFilesHandler;
 import java.io.File;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import net.sf.rails.common.ConfigManager;
+import net.sf.rails.ui.swing.AutoSaveLoadDialog;
 import net.sf.rails.ui.swing.GameSetupController;
 
 public class RunGame {
+
+    private static final Logger log = LoggerFactory.getLogger(RunGame.class);
 
     public static void main(String[] args) {
 
         // Initialize configuration
         ConfigManager.initConfiguration(false);
-        
-        int nargs = 0;
+
+        String fileName = null;
+
         if (args != null && args.length > 0) {
-            nargs = args.length;
-            System.out.println("Number of args: "+nargs);
             for (String arg : args) {
                 System.out.println ("Arg: "+arg);
             }
+            fileName = args[0];
         }
 
+        Desktop.getDesktop().setOpenFileHandler(e -> {
+            for (File file : e.getFiles() ) {
+                // open the file
+                log.warn("found file: " + file.toString());
+                //GameLoader.loadAndStartGame(file);
+                // break out as we only handle one game at a time...
+                break;
+            }
+        });
+
         // currently we only start one game
-        if (nargs >= 1) {
-            loadGame(args[0]);
+        if ( fileName != null ) {
+            GameLoader.loadAndStartGame(new File(fileName));
         } else {
             /* Start the rails.game selector, which will do all the rest. */
             GameSetupController.Builder setupBuilder = GameSetupController.builder();
             setupBuilder.start();
         }
-    }
-
-    private static void loadGame (String filePath) {
-        File gameFile = new File(filePath);
-        GameLoader.loadAndStartGame(gameFile);
     }
 }
